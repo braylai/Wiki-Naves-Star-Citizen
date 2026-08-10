07310c0ae6a5e1e6fd57acdbbaf449b676c459a6
Brayan <brayan747k@gmail.com>
2026-08-10 16:04:26 +0200

    pagina

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const fn = () => {
+    const miString = "soy un string"
+    let b = miString + "muy feliz"
+    console.log(b)
+}
+
+fn()
+
+const clickeado = () => {
+    let a = "brayan feliz"
+
+    console.log(a)
+}
